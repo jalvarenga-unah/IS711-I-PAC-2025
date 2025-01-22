ea0bda4b60fab303c98b5731527618209e8e9d30
jalvarenga11 <116615884+jalvarenga11@users.noreply.github.com>
2025-01-22 17:49:03 -0600

    Introduccion a variables en js

---
+++ intro-js/01-variables.js
@@ -0,0 +1,11 @@
+// ? Global Scope / Scope (alcance)
+// variables
+
+// ❌ no deberíamos definir una variable
+
+//define las variables a nivel Global
+var nombre = 'Juan'
+var nombre = 'Pedro' // <- sobreescribiendo el valor de la variable orginal
+
+//muestra un mensaje en consola
+console.log(nombre)
