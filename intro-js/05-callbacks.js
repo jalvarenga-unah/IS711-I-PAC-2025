66eabafd878ffcafb646e92c23f9fde41a022723
jalvarenga11 <116615884+jalvarenga11@users.noreply.github.com>
2025-01-30 17:49:48 -0600

    manejo de funciones e introducción a las promesas

--- intro-js/05-callbacks.js
+++ intro-js/05-callbacks.js
@@ -6,11 +6,79 @@ const lista = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 //     console.log(lista[i]);
 // }
 
-// lista.forEach(callback)
+// lista.forEach(callback) // <- función pasada por referencia
 lista.forEach(function (value) {
-    console.log(value ** 3);
+    // console.log(value ** 3);
 })
 
 function callback(value, index, array) {
     console.log(`${index}: ${value} - ${array}`);
-}   
+}
+
+//? continuación
+// for (const valor of lista) {
+//     console.log(valor);
+// } 
+
+// una copia de la lista original
+const nuevaLista = lista.map((valor) => {
+    // console.log(valor);
+    // return valor // devuelve un nuevo elemento al array que va a retornar el map
+    if (valor % 2 === 0) {
+        return valor
+    }
+
+})
+
+console.log(nuevaLista);
+
+
+const pares = lista.filter((valor) => {
+    return valor % 2 === 0
+})
+
+console.log(pares);
+
+const postres = [{
+    nombre: 'Torta de chocolate',
+    precio: 100
+}, {
+    nombre: 'Pie de limón',
+    precio: 50
+}, {
+    nombre: 'Gelatina',
+    precio: 20
+}]
+
+// const evaluacion = postres.filter((postre) => {
+//     return postre.precio < 40
+// })
+
+const evaluacion = postres.every((postre) => {
+    return postre.precio < 40
+})
+
+// muta o transforma el array original
+lista.sort((a, b) => {
+    return a - b
+})
+
+const ordenados = lista.toSorted((a, b) => a - b); // no muta el array original
+
+console.log(ordenados);
+console.log(evaluacion ? "si hay" : 'No hay postres baratos');
+
+
+const saludo = () => {
+    console.log(`Hola!!`);
+}
+
+const persona = {
+    'nombre': 'Juan',
+    'apellido': 'Perez',
+    'edad': 30,
+    'accion': saludo
+}
+
+persona.accion();
+
---
+++ intro-js/06-promesas.js
@@ -0,0 +1,13 @@
+
+
+function suma(a, b) {
+    return a + b
+}
+
+// Promesas -> una tarea que se resuelve o falla en el futuro
+// es una función que recibe un callbac y me permite
+// trabajar con tareas asincronas
+
+//de forma asincrona, que no bloquea el hilo principal
+
+// de forma sincrona, que detiene el hilo principal
