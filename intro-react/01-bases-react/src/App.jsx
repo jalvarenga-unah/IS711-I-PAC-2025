6e1c77dd7fee75028fcffd4066ce28e340cc0565
jalvarenga11 <116615884+jalvarenga11@users.noreply.github.com>
2025-04-03 17:51:10 -0600

    manejo de estado

--- intro-react/01-bases-react/src/App.jsx
+++ intro-react/01-bases-react/src/App.jsx
@@ -1,10 +1,10 @@
 import { Producto } from './components/Producto.jsx'
-
-
+import './styles/app.css'
 // componente App -> Una etiqueta especial de HTML
 export const App = () => {
 
   const producto3 = {
+    title: "Producto 3",
     price: 900,
     description: "Descripción del prodcuto 3",
     image: "https://unavatar.io/github/jalvarenga-unah"
@@ -12,32 +12,26 @@ export const App = () => {
 
   return (
     <>
-      <h1>Mi primer componente</h1>
+      <h2>Mi primer componente</h2>
 
-      <Producto {...producto3}>
-        <h2>Producto 3</h2>
-      </Producto>
+      <section className='container'>
 
-      <Producto
-        price={300}
-        description="Descripción del prodcuto 1"
-        image="https://unavatar.io/github/jalvarenga-unah" >
+        <Producto {...producto3} />
 
-        <h2>Producto 1</h2>
-
-      </Producto>
-
-      <Producto
-
-        price={600}
-        description="Descripción del prodcuto 1"
-        image="https://unavatar.io/github/jalvarenga-unah">
+        <Producto
+          title="Producto 1"
+          price={300}
+          description="Descripción del prodcuto 1"
+          image="https://unavatar.io/github/jalvarenga-unah" />
 
 
-        <h2>Producto 2</h2>
+        <Producto
+          title="Producto 2"
+          price={600}
+          description="Descripción del prodcuto 1"
+          image="https://unavatar.io/github/jalvarenga-unah" />
+      </section>
 
-
-      </Producto>
 
 
     </>
--- intro-react/01-bases-react/src/components/Producto.jsx
+++ intro-react/01-bases-react/src/components/Producto.jsx
@@ -1,21 +1,43 @@
-
+import { useState } from 'react'
+import { LikeIcon } from '../icons/LikeIcon'
+import '../styles/producto.css'
 
 //recibiendo props
-export const Producto = ({ children, image, description, price }) => {
+export const Producto = ({ title, image, description, price }) => {
+    // children -> Prop especial de React equivalente a "slot"
+
+    //el estado es a nivel de componente
+    const [isLike, setIsLike] = useState(false) // un estado para el componente
 
     //? Las props deben ser Inmutables
     // const newPrice = `HNL ${price}`
     return (
         <>
-            <article>
+            <article className='card'>
                 <header>
-                    <img src={image} alt="" />
-                    {children}
+                    <img className="img-avatar" src={image} alt="" />
+                    <h2 className="title">{title}</h2>
                 </header>
                 <div>
                     <p>{description}</p>
                     <p>HNL {price}</p>
                 </div>
+
+                <div>
+                    <button
+                        onClick={() => {
+                            // hacer una petición al servidor
+
+                            //actualizar el estado y la UI
+                            setIsLike(!isLike)
+                        }}
+                        className="btn">
+                        <span className="">
+                            <LikeIcon isLiked={isLike} />
+                        </span>
+                        Agregar a favoritos</button>
+                </div>
+
             </article>
         </>
     )
